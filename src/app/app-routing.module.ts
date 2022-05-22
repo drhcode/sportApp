cc1f6bdab813ab0809efb78337c550863a281afd
drhcode <drhcode1@gmail.com>
2022-05-22 10:59:59 +0200

    first changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ExampleComponent } from './example/example.component';
+import { HomeComponent } from './section/home/home.component';
+import { NewsComponent } from './section/news/news.component';
+import { ResultsComponent } from './section/results/results.component';
+import { TeamsComponent } from './section/teams/teams.component';
 
-const routes: Routes = [{ path: 'example', component: ExampleComponent }];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'teams', component: TeamsComponent },
+  { path: 'news', component: NewsComponent },
+  { path: 'results', component: ResultsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ExampleComponent } from './example/example.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HomeComponent } from './section/home/home.component';
+import { TeamsComponent } from './section/teams/teams.component';
+import { ResultsComponent } from './section/results/results.component';
+import { NewsComponent } from './section/news/news.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ExampleComponent
+    ExampleComponent,
+    HomeComponent,
+    TeamsComponent,
+    ResultsComponent,
+    NewsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
