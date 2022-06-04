d8f67d714cdf41601741e03415a330d84d4f86d1
drhcode <drhcode1@gmail.com>
2022-06-04 15:32:43 +0200

    some home fixes

--- src/app/section/results/results.component.ts
+++ src/app/section/results/results.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import matchesData from 'src/assets/matches.json';
+
+interface Matches {
+  time: String;
+  // name: String;
+  score: number;
+  logo: String;
+  location: String;
+}
 
 @Component({
   selector: 'app-results',
   templateUrl: './results.component.html',
-  styleUrls: ['./results.component.scss']
+  styleUrls: ['./results.component.scss'],
 })
-export class ResultsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class ResultsComponent {
+  matches = matchesData;
 }
