6dd7d2e555a2eaa4281af92b9b2468264f68203b
drhcode <drhcode1@gmail.com>
2022-06-12 11:53:54 +0200

    ..

--- src/app/services/creategame.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@angular/core';
-import matchesData from 'src/assets/matches.json';
-import { Observable, of } from 'rxjs';
-import { Match } from 'src/app/shared/models/match';
-
-@Injectable({
-  providedIn: 'root',
-})
-export class CreateGameService {}
